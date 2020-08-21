0799574f7a62d95ef41913da55e70ef00c28550b
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-08-22 01:59:24 +0500

    add home screen

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>Home Screen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    
+});
+
+export default HomeScreen;
